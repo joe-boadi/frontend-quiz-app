25ba69e020c6c095c5954abf0ed306d06ebfdadb
Kwadjo Boadi-Mantey <kwadjo.boadi-mantey@amalitech.com>
2024-12-10 14:55:16 +0000

    refactor usePersistedState hook

---
+++ src/features/Functions.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+  const usePersistedState = ( key:string,value:unknown ) => {
+    const [state, setState] = useState(() => {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : value;
+    });
+
+    useEffect(() => {
+      localStorage.setItem(key, JSON.stringify(state));
+    }, [state, key]);
+  
+    return [state, setState] as const;
+  }
+
+export  { usePersistedState };
